127f9c00603f148d9ed2162d9455cd3aa55566d7
KedarRam <10015197@sbstudents.org>
2017-01-25 20:02:31 -0500

    Update kingpinsdb.sql

--- db/kingpinsdb.sql
+++ db/kingpinsdb.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `kingpinsdb`
 --
-CREATE DATABASE IF NOT EXISTS `kingpinsdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `kingpinsdb2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `kingpinsdb`;
 -- --------------------------------------------------------
 
